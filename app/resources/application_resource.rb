a97b47352bd11b158a07798d4b7e653504666666
Keith Bauson <kwbauson@gmail.com>
2021-04-09 00:16:35 -0400

    remove models and controllers

--- app/resources/application_resource.rb
+++ app/resources/application_resource.rb
@@ -1,11 +1,3 @@
-# ApplicationResource is similar to ApplicationRecord - a base class that
-# holds configuration/methods for subclasses.
-# All Resources should inherit from ApplicationResource.
-class ApplicationResource < Graphiti::Resource
-  # Use the ActiveRecord Adapter for all subclasses.
-  # Subclasses can still override this default.
+class ApplicationResource < Graphiti::Rails::Auto::Resource
   self.abstract_class = true
-  self.adapter = Graphiti::Adapters::ActiveRecord
-  self.base_url = Rails.application.routes.default_url_options[:host]
-  self.endpoint_namespace = '/api/v1'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   scope path: ApplicationResource.endpoint_namespace, defaults: { format: :jsonapi } do
     mount VandalUi::Engine, at: '/vandal'
+    PostResource
     resources :posts
   end
 end
