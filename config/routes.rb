ed6f2609cdf50ffbcbb66b7acb473b3e23235272
Keith Bauson <kwbauson@gmail.com>
2021-04-08 22:30:29 -0400

    add vandal and remove a bunch of deps

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope path: ApplicationResource.endpoint_namespace, defaults: { format: :jsonapi } do
+    mount VandalUi::Engine, at: '/vandal'
+    resources :posts
+  end
 end
